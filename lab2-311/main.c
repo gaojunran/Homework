0c310ef5aeabaa8ee34ce705cd340a52e31f9b06
gaojunran <nebula2021@126.com>
2025-04-19 22:08:03 +0800

    (lab2-311) 准备提交

--- lab2-311/main.c
+++ lab2-311/main.c
@@ -1,192 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // Define a structure to represent a transition
 typedef struct {
-  int fromState;
-  char input;
-  int toState;
+    int fromState;
+    char input;
+    int toState;
 } Transition;
 
 // Define a structure to represent an NFA
 typedef struct {
-  int cardinality; // Number of states
-  char alphabet[256]; // Set of input symbols
-  int finalStates[100]; // Set of final states
-  int finalStateCount; // Number of final states
-  int startState; // Start state
-  Transition transitions[1000]; // Set of transitions
-  int transitionCount; // Number of transitions
+    int cardinality; // Number of states
+    char alphabet[256]; // Bitmap of input symbols
+    int finalStates[100]; // Set of final states
+    int finalStateCount;
+    int startState;
+    Transition transitions[1000];
+    int transitionCount;
 } NFA;
 
 // Define a structure to represent a DFA
 typedef struct {
-  int cardinality; // Number of states
-  char alphabet[256]; // Set of input symbols
-  int finalStates[100]; // Set of final states
-  int finalStateCount; // Number of final states
-  int startState; // Start state
-  Transition transitions[1000]; // Set of transitions
-  int transitionCount; // Number of transitions
+    int cardinality;
+    char alphabet[256]; // Bitmap of input symbols
+    int finalStates[100];
+    int finalStateCount;
+    int startState;
+    Transition transitions[1000];
+    int transitionCount;
 } DFA;
 
-// Function to check if a character is in the alphabet
 int isInAlphabet(char alphabet[256], char c) {
-  return alphabet[(int)c] == 1;
+    return alphabet[(unsigned char)c] == 1;
 }
 
-// Function to add a character to the alphabet
 void addToAlphabet(char alphabet[256], char c) {
-  alphabet[(int)c] = 1;
+    alphabet[(unsigned char)c] = 1;
 }
 
-// Function to check if a state is in the final states
 int isFinalState(int finalStates[100], int finalStateCount, int state) {
-  for (int i = 0; i < finalStateCount; i++) {
-      if (finalStates[i] == state) {
-          return 1;
-      }
-  }
-  return 0;
+    for (int i = 0; i < finalStateCount; i++) {
+        if (finalStates[i] == state) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
-// Function to convert an NFA to a DFA
 DFA convertNFAToDFA(NFA nfa) {
-  DFA dfa;
-  int stateMapping[1000][100]; // Maps NFA state sets to DFA state IDs
-  int stateMappingCount = 0;
-  int queue[1000][100]; // Queue to process subset states
-  int queueFront = 0, queueRear = 0;
+    DFA dfa;
+    memset(&dfa, 0, sizeof(DFA));
+    memcpy(dfa.alphabet, nfa.alphabet, 256);
 
-  // Initialize the start state of the DFA
-  int startSet[100] = {0};
-  startSet[0] = nfa.startState;
-  queue[queueRear++][0] = nfa.startState;
-  stateMapping[stateMappingCount++][0] = nfa.startState;
+    int visited[1000][100] = {0};
+    int visitedCount = 0;
+    int queue[1000][100] = {0};
+    int queueSize[1000] = {0};
+    int queueFront = 0, queueRear = 0;
+    int dfaStateCount = 0;
 
-  int dfaFinalStates[100] = {0};
-  int dfaFinalStateCount = 0;
-  memcpy(dfa.alphabet, nfa.alphabet, 256);
-  int stateCounter = 1;
+    int startSet[100] = {nfa.startState};
+    int startSetSize = 1;
+    memcpy(queue[queueRear], startSet, sizeof(int) * startSetSize);
+    queueSize[queueRear++] = startSetSize;
 
-  while (queueFront < queueRear) {
-      int currentStateSet[100];
-      memcpy(currentStateSet, queue[queueFront++], sizeof(currentStateSet));
-      int dfaStateId = stateMappingCount - 1;
+    int stateId[1000][100] = {0};
+    int stateSize[1000] = {0};
+    memcpy(stateId[dfaStateCount], startSet, sizeof(int) * startSetSize);
+    stateSize[dfaStateCount++] = startSetSize;
 
-      // Check if the current DFA state contains any NFA final states
-      for (int i = 0; i < nfa.cardinality; i++) {
-          if (isFinalState(nfa.finalStates, nfa.finalStateCount, currentStateSet[i])) {
-              dfaFinalStates[dfaFinalStateCount++] = dfaStateId;
-              break;
-          }
-      }
+    dfa.startState = 0;
 
-      // Process each input symbol
-      for (int i = 0; i < 256; i++) {
-          if (isInAlphabet(nfa.alphabet, i)) {
-              int nextStateSet[100] = {0};
-              int nextStateCount = 0;
+    while (queueFront < queueRear) {
+        int* currentSet = queue[queueFront];
+        int currentSize = queueSize[queueFront++];
+        int currentDfaState = -1;
 
-              // Compute the set of next NFA states for the current input symbol and current NFA state
-              for (int j = 0; j < nfa.cardinality; j++) {
-                  for (int k = 0; k < nfa.transitionCount; k++) {
-                      if (nfa.transitions[k].fromState == currentStateSet[j] && nfa.transitions[k].input == i) {
-                          nextStateSet[nextStateCount++] = nfa.transitions[k].toState;
-                      }
-                  }
-              }
+        // Find DFA state id
+        for (int i = 0; i < dfaStateCount; i++) {
+            if (stateSize[i] == currentSize &&
+                memcmp(stateId[i], currentSet, sizeof(int) * currentSize) == 0) {
+                currentDfaState = i;
+                break;
+            }
+        }
 
-              // If there are next states, add a transition to the DFA
-              if (nextStateCount > 0) {
-                  int nextStateSetHash = 0;
-                  for (int j = 0; j < nextStateCount; j++) {
-                      nextStateSetHash += nextStateSet[j];
-                  }
+        // Final state check
+        for (int i = 0; i < currentSize; i++) {
+            if (isFinalState(nfa.finalStates, nfa.finalStateCount, currentSet[i])) {
+                if (!isFinalState(dfa.finalStates, dfa.finalStateCount, currentDfaState)) {
+                    dfa.finalStates[dfa.finalStateCount++] = currentDfaState;
+                }
+                break;
+            }
+        }
 
-                  int found = 0;
-                  for (int j = 0; j < stateMappingCount; j++) {
-                      int stateSetHash = 0;
-                      for (int k = 0; k < nfa.cardinality; k++) {
-                          stateSetHash += stateMapping[j][k];
-                      }
-                      if (stateSetHash == nextStateSetHash) {
-                          found = 1;
-                          dfa.transitions[dfa.transitionCount++] = (Transition){dfaStateId, i, j};
-                          break;
-                      }
-                  }
+        for (int c = 0; c < 256; c++) {
+            if (!isInAlphabet(nfa.alphabet, c)) continue;
 
-                  if (!found) {
-                      stateMapping[stateMappingCount++][0] = nextStateSet[0];
-                      queue[queueRear++][0] = nextStateSet[0];
-                      dfa.transitions[dfa.transitionCount++] = (Transition){dfaStateId, i, stateCounter++};
-                  }
-              }
-          }
-      }
-  }
+            int nextSet[100] = {0};
+            int nextSetSize = 0;
 
-  dfa.cardinality = stateCounter;
-  dfa.startState = 0;
-  dfa.finalStateCount = dfaFinalStateCount;
-  memcpy(dfa.finalStates, dfaFinalStates, sizeof(dfaFinalStates));
-  return dfa;
+            for (int i = 0; i < currentSize; i++) {
+                for (int j = 0; j < nfa.transitionCount; j++) {
+                    if (nfa.transitions[j].fromState == currentSet[i] &&
+                        nfa.transitions[j].input == c) {
+                        int exists = 0;
+                        for (int k = 0; k < nextSetSize; k++) {
+                            if (nextSet[k] == nfa.transitions[j].toState) {
+                                exists = 1;
+                                break;
+                            }
+                        }
+                        if (!exists) {
+                            nextSet[nextSetSize++] = nfa.transitions[j].toState;
+                        }
+                    }
+                }
+            }
+
+            if (nextSetSize == 0) continue;
+
+            // Check if this set already exists
+            int existingState = -1;
+            for (int i = 0; i < dfaStateCount; i++) {
+                if (stateSize[i] == nextSetSize &&
+                    memcmp(stateId[i], nextSet, sizeof(int) * nextSetSize) == 0) {
+                    existingState = i;
+                    break;
+                }
+            }
+
+            if (existingState == -1) {
+                memcpy(stateId[dfaStateCount], nextSet, sizeof(int) * nextSetSize);
+                stateSize[dfaStateCount] = nextSetSize;
+                memcpy(queue[queueRear], nextSet, sizeof(int) * nextSetSize);
+                queueSize[queueRear++] = nextSetSize;
+                existingState = dfaStateCount++;
+            }
+
+            dfa.transitions[dfa.transitionCount++] = (Transition){currentDfaState, (char)c, existingState};
+        }
+    }
+
+    dfa.cardinality = dfaStateCount;
+    return dfa;
 }
 
-// Function to output the DFA structure
-void outputDFA(DFA dfa) {
-  printf("%d\n", dfa.cardinality);
-  for (int i = 0; i < 256; i++) {
-      if (isInAlphabet(dfa.alphabet, i)) {
-          putchar(i);
-      }
-  }
-  printf("\n");
-  for (int i = 0; i < dfa.finalStateCount; i++) {
-      printf("%d ", dfa.finalStates[i]);
-  }
-  printf("\n");
-  printf("%d\n", dfa.startState);
-  for (int i = 0; i < dfa.transitionCount; i++) {
-      printf("%d %c %d\n", dfa.transitions[i].fromState, dfa.transitions[i].input, dfa.transitions[i].toState);
-  }
+void outputDFA(DFA dfa, char alphabetList[], int alphabetSize) {
+    printf("%d\n", dfa.cardinality);
+
+    for (int i = 0; i < alphabetSize; i++) {
+        putchar(alphabetList[i]);
+    }
+    printf("\n");
+
+    printf("%d ", dfa.finalStateCount);
+    for (int i = 0; i < dfa.finalStateCount; i++) {
+        printf("%d ", dfa.finalStates[i]);
+    }
+    printf("\n");
+
+    printf("%d\n", dfa.startState);
+    for (int i = 0; i < dfa.transitionCount; i++) {
+        printf("%d %c %d\n", dfa.transitions[i].fromState,
+               dfa.transitions[i].input, dfa.transitions[i].toState);
+    }
 }
 
 int main() {
-  NFA nfa;
-  printf("Enter number of states:\n");
-  scanf("%d", &nfa.cardinality);
+    NFA nfa;
+    memset(&nfa, 0, sizeof(NFA));
+    char alphabetInput[256];
+    char alphabetList[256];
+    int alphabetSize = 0;
 
-  printf("Enter alphabet (characters without spaces, e.g., abc):\n");
-  char alphabetInput[256];
-  scanf("%s", alphabetInput);
-  memset(nfa.alphabet, 0, 256);
-  for (int i = 0; alphabetInput[i] != '\0'; i++) {
-      addToAlphabet(nfa.alphabet, alphabetInput[i]);
-  }
+    printf("Enter number of states:\n");
+    scanf("%d", &nfa.cardinality);
 
-  printf("Enter number of final states followed by the final states (e.g., 2 1 3):\n");
-  scanf("%d", &nfa.finalStateCount);
-  for (int i = 0; i < nfa.finalStateCount; i++) {
-      scanf("%d", &nfa.finalStates[i]);
-  }
+    printf("Enter alphabet (characters without spaces, e.g., abc):\n");
+    scanf("%s", alphabetInput);
+    memset(nfa.alphabet, 0, 256);
+    for (int i = 0; alphabetInput[i] != '\0'; i++) {
+        char c = alphabetInput[i];
+        if (!isInAlphabet(nfa.alphabet, c)) {
+            addToAlphabet(nfa.alphabet, c);
+            alphabetList[alphabetSize++] = c;
+        }
+    }
 
-  printf("Enter start state:\n");
-  scanf("%d", &nfa.startState);
+    printf("Enter number of final states followed by the final states (e.g., 2 1 3):\n");
+    scanf("%d", &nfa.finalStateCount);
+    for (int i = 0; i < nfa.finalStateCount; i++) {
+        scanf("%d", &nfa.finalStates[i]);
+    }
 
-  printf("Enter transitions (format: fromState inputSymbol toState), one per line. Enter an empty line to finish:\n");
-  nfa.transitionCount = 0;
-  while (1) {
-      char line[256];
-      if (fgets(line, sizeof(line), stdin) == NULL || strcmp(line, "\n") == 0) {
-          break;
-      }
-      int fromState, toState;
-      char input;
-      sscanf(line, "%d %c %d", &fromState, &input, &toState);
-      nfa.transitions[nfa.transitionCount++] = (Transition){fromState, input, toState};
-  }
+    printf("Enter start state:\n");
+    scanf("%d", &nfa.startState);
 
-  DFA dfa = convertNFAToDFA(nfa);
-  outputDFA(dfa);
+    printf("Enter transitions (format: fromState inputSymbol toState), one per line. Enter an empty line to finish:\n");
+    getchar(); // consume newline after scanf
+    char line[256];
+    nfa.transitionCount = 0;
+    while (1) {
+        if (!fgets(line, sizeof(line), stdin) || strcmp(line, "\n") == 0) break;
 
-  return 0;
+        int fromState, toState;
+        char input;
+        if (sscanf(line, "%d %c %d", &fromState, &input, &toState) == 3) {
+            nfa.transitions[nfa.transitionCount++] = (Transition){fromState, input, toState};
+        }
+    }
+
+    DFA dfa = convertNFAToDFA(nfa);
+    outputDFA(dfa, alphabetList, alphabetSize);
+
+    return 0;
 }
